ab337c725ce3d92a6a984abbf3d4582a07157023
dexteroustechnology <134916834+kiranaworld@users.noreply.github.com>
2024-08-13 16:34:35 +0530

    r
    r

--- src/app/port-folio/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PortFolio = () => {
-  return (
-    <div>PortFolio page</div>
-  )
-}
-
-export default PortFolio
--- src/app/real-estate-app-developement/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import GeneralWebsites from "@/components/Portfolio/GeneralWebsites";
-const page = () => {
-  return <GeneralWebsites />;
-};
-
-export default page;
